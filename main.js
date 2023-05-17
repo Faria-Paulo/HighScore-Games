f300ee4d79638fb2c90c13bed0af947159cb37df
Faria-Paulo <102263955+Faria-Paulo@users.noreply.github.com>
2023-05-17 16:33:42 -0300

    new games add

--- main.js
+++ main.js
@@ -28,17 +28,17 @@ function trocaBannerDoodleJump(){
 }
 
 function trocaBannerFlappyBird(){
-    document.getElementById('banner').style.backgroundImage="url(img/posters/flappy_bird2.jpg)";
+    document.getElementById('banner').style.backgroundImage="url(img/posters/flappy_bird4.jpg)";
     document.querySelector('.banner_buttons').style.display="block";
-    document.querySelector('.banner_title').innerHTML="Flappy Bird";
+    document.querySelector('.banner_title').innerHTML="<br><br>";
     document.querySelector('.banner_title').style.fontFamily='arial';
     document.querySelector('.banner_title').style.color="white";
     document.querySelector('.banner_title').style.fontWeight="200";
 
-    document.querySelector('.banner_discription').innerHTML="O famoso Flappy Bird, tão estressante quanto divertido!";
+    document.querySelector('.banner_discription').innerHTML="<br>O famoso Flappy Bird, tão estressante quanto divertido!";
     document.querySelector('.banner_discription').style.fontFamily="arial";
     document.querySelector('.banner_discription').style.color="white";
-    document.querySelector('.banner_discription').style.fontWeight="300";
+    document.querySelector('.banner_discription').style.fontWeight="600";
 
     const a = document.querySelector('#game_link');
     const b = document.querySelector('#game_history');
@@ -47,26 +47,101 @@ function trocaBannerFlappyBird(){
 }
 
 function trocaBannerPong(){
-    document.getElementById('banner').style.backgroundImage="url(img/posters/pong2.jpg)";
+    document.getElementById('banner').style.backgroundImage="url(img/posters/pong4.png)";
     document.querySelector('.banner_buttons').style.display="block";
+
+    document.querySelector('.banner_title').innerHTML="<br><br><br>";
+    document.querySelector('.banner_title').style.fontFamily='arial';
+    document.querySelector('.banner_title').style.color="white";
+    document.querySelector('.banner_title').style.fontWeight="200";
+
+    document.querySelector('.banner_discription').innerHTML="<br>Conhecido por ser o primeiro jogo eletrônico criado, jogue agora!";
+    document.querySelector('.banner_discription').style.fontFamily="arial";
+    document.querySelector('.banner_discription').style.color="white";
+    document.querySelector('.banner_discription').style.fontWeight="600";
+
+    const a = document.querySelector('#game_link');
+    const b = document.querySelector('#game_history');
+    a.href  = "pong/play/pong_play.html";
+    b.href  = "doodle_jump/play/story/doodle_jump_history.html";
 }
 
 function trocaBannerSnake(){
-    document.getElementById('banner').style.backgroundImage="url(img/posters/snake2.png)";
+    document.getElementById('banner').style.backgroundImage="url(img/posters/snake4.png)";
     document.querySelector('.banner_buttons').style.display="block";
+
+    document.querySelector('.banner_title').innerHTML="<br><br><br>";
+    document.querySelector('.banner_title').style.fontFamily='arial';
+    document.querySelector('.banner_title').style.color="white";
+    document.querySelector('.banner_title').style.fontWeight="200";
+
+    document.querySelector('.banner_discription').innerHTML="<br>Lembra do Nokia e seu jogo da cobrinha? Mate essa saudade!";
+    document.querySelector('.banner_discription').style.fontFamily="arial";
+    document.querySelector('.banner_discription').style.color="white";
+    document.querySelector('.banner_discription').style.fontWeight="600";
+
+    const a = document.querySelector('#game_link');
+    const b = document.querySelector('#game_history');
+    a.href  = "snake/play/snake.html";
+    b.href  = "doodle_jump/play/story/doodle_jump_history.html";
 }
 
 function trocaBannerSpaceInvaders(){
-    document.getElementById('banner').style.backgroundImage="url(img/posters/space_invaders3.jpg)";
+    document.getElementById('banner').style.backgroundImage="url(img/posters/space_invaders3.png)";
     document.querySelector('.banner_buttons').style.display="block";
+
+    document.querySelector('.banner_title').innerHTML="<br><br><br>";
+    document.querySelector('.banner_title').style.fontFamily='arial';
+    document.querySelector('.banner_title').style.color="white";
+    document.querySelector('.banner_title').style.fontWeight="200";
+
+    document.querySelector('.banner_discription').innerHTML="<br>Quando se pensa em arcade, esse é o jogo que vem em mente!";
+    document.querySelector('.banner_discription').style.fontFamily="arial";
+    document.querySelector('.banner_discription').style.color="white";
+    document.querySelector('.banner_discription').style.fontWeight="600";
+
+    const a = document.querySelector('#game_link');
+    const b = document.querySelector('#game_history');
+    a.href  = "space_invaders/play/index.html";
+    b.href  = "doodle_jump/play/story/doodle_jump_history.html";
 }
 
 function trocaBannerTetris(){
-    document.getElementById('banner').style.backgroundImage="url(img/posters/tetris2.png)";
+    document.getElementById('banner').style.backgroundImage="url(img/posters/tetris6.png)";
     document.querySelector('.banner_buttons').style.display="block";
+
+    document.querySelector('.banner_title').innerHTML="<br><br><br>";
+    document.querySelector('.banner_title').style.fontFamily='arial';
+    document.querySelector('.banner_title').style.color="white";
+    document.querySelector('.banner_title').style.fontWeight="200";
+
+    document.querySelector('.banner_discription').innerHTML="<br>Divertidamente viciante, clássico dos clássicos!";
+    document.querySelector('.banner_discription').style.fontFamily="arial";
+    document.querySelector('.banner_discription').style.color="white";
+    document.querySelector('.banner_discription').style.fontWeight="600";
+
+    const a = document.querySelector('#game_link');
+    const b = document.querySelector('#game_history');
+    a.href  = "tetris/play/index.html";
+    b.href  = "tetris/play/index.html";
 }
 
 function trocaBannerFruitNinja(){
     document.getElementById('banner').style.backgroundImage="url(img/posters/fruit_ninja2.png)";
     document.querySelector('.banner_buttons').style.display="block";
+
+    document.querySelector('.banner_title').innerHTML="<br><br><br>";
+    document.querySelector('.banner_title').style.fontFamily='arial';
+    document.querySelector('.banner_title').style.color="white";
+    document.querySelector('.banner_title').style.fontWeight="200";
+
+    document.querySelector('.banner_discription').innerHTML="<br>O vicio de todos que um dia tiveram um Galaxy Pocket!";
+    document.querySelector('.banner_discription').style.fontFamily="arial";
+    document.querySelector('.banner_discription').style.color="white";
+    document.querySelector('.banner_discription').style.fontWeight="600";
+
+    const a = document.querySelector('#game_link');
+    const b = document.querySelector('#game_history');
+    a.href  = "fruit_ninja/play/index.html";
+    b.href  = "doodle_jump/play/story/doodle_jump_history.html";
 }
