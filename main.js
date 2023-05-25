2102b55e212f181e1451c734e41c2275d13c8d36
Faria-Paulo <102263955+Faria-Paulo@users.noreply.github.com>
2023-05-25 13:27:53 -0300

    click and drag

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
+/*  Constantes e variáveis  */
+
+
+
+/*  Escurecer o Nav  */
 const nav = document.getElementById('nav');
 
 
 
-
-/*  slider horizontal  */
+/*  slider horizontal arrastar */
 const slider = document.querySelector('.row_posters');
+const slider2 = document.querySelector('.row_posters2');
+const slider3 = document.querySelector('.row_posters3');
 let isDown = false;
 let startX;
 let ScrollLeft;
-/*  slider horizontal  */
+
+
+/*  Funções  */
 
 
 
-
-
+/*  Escurecer o Nav  */
 window.addEventListener('scroll', () =>{
     if(window.scrollY >=100){
         nav.classList.add('nav_black');
@@ -22,11 +29,14 @@ window.addEventListener('scroll', () =>{
     }
 })
 
-/*  Slider horizontal  */
 
-slider.addEventListener('mousedown', () => {
+/*  Slider horizontal arrastar (Primeira div) */
+slider.addEventListener('mousedown', (e) => {
     isDown = true;
     slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    console.log(startX);
 });
 
 slider.addEventListener('mouseleave', () => {
@@ -40,16 +50,76 @@ slider.addEventListener('mouseup', () => {
     slider.classList.remove('active');
 });
 
-slider.addEventListener('mousemove', () => {
+slider.addEventListener('mousemove', (e) => {
     if(!isDown) return;
-    console.count(isDown);
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x -startX) * 3; //caso esteja a rolagem esteja muito rápida, tirar o multiplicador 3
+    slider.scrollLeft = scrollLeft - walk;
+});
+
+
+/*  Slider horizontal arrastar (Segunda div)  const slider2 e class row_posters2*/
+slider2.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider2.classList.add('active');
+    startX = e.pageX - slider2.offsetLeft;
+    scrollLeft = slider2.scrollLeft;
+    console.log(startX);
+});
+
+slider2.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider2.classList.remove('active');
     
+});
+
+slider2.addEventListener('mouseup', () => {
+    isDown = false;
+    slider2.classList.remove('active');
+});
+
+slider2.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider2.offsetLeft;
+    const walk = (x -startX) * 3; //caso esteja a rolagem esteja muito rápida, tirar o multiplicador 3
+    slider2.scrollLeft = scrollLeft - walk;
+});
+
+/*  Slider horizontal arrastar (Terceira div) const slider3 e class row_posters3  */
+slider3.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider3.classList.add('active');
+    startX = e.pageX - slider3.offsetLeft;
+    scrollLeft = slider3.scrollLeft;
+    console.log(startX);
+});
+
+slider3.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider3.classList.remove('active');
     
+});
+
+slider3.addEventListener('mouseup', () => {
+    isDown = false;
+    slider3.classList.remove('active');
+});
+
+slider3.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider3.offsetLeft;
+    const walk = (x -startX) * 3; //caso esteja a rolagem esteja muito rápida, tirar o multiplicador 3
+    slider3.scrollLeft = scrollLeft - walk;
 });
 
 
 
 
+
+/*  Funções para troca de banner de destaque  */
 function trocaBannerBlackJack(){
     document.getElementById('banner').style.backgroundImage="url(img/posters/black_jack2.png)";
     document.querySelector('.banner_buttons').style.display="block";
@@ -282,4 +352,54 @@ function trocaBannerPacMan(){
     const b = document.querySelector('#history_link');
     a.href  = "pac-man/pacman/index.html";
     b.href  = "fruit_ninja/story/story.html";
+}
+
+/*  função clique e jogue  */
+
+function jogarBlackJack(){
+    window.open("black_jack/index.html");
+}
+
+function jogarDelta(){
+    window.open("delta_master/index.html");
+}
+
+function jogarDoodleJump(){
+    window.open("doodle_jump/play/doodle_jump_play.html");
+}
+
+function jogarFlappyBird(){
+    window.open("flappy_bird/play/flappy_bird_play.html");
+}
+
+function jogarFruitNinja(){
+    window.open("fruit_ninja/play/index.html");
+}
+
+function jogarPacMan(){
+    window.open("pac-man/pacman/index.html");
+}
+
+function jogarPong(){
+    window.open("pong/play/pong_play.html");
+}
+
+function jogarRaceMaster(){
+    window.open("race_master/v4.final.html");
+}
+
+function jogarRoadFighter(){
+    window.open("road_fighter/game.html");
+}
+
+function jogarSnake(){
+    window.open("snake/play/snake.html");
+}
+
+function jogarSpaceInvaders(){
+    window.open("space_invaders/play/index.html");
+}
+
+function jogarTetris(){
+    window.open("tetris/play/index.html");
 }
